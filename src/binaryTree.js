c88d087955e24d601503b1de08ff30925c1fc191
giprim <giprim@DESKTOP-D9QG0ED.localdomain>
2021-12-09 13:08:11 +0100

    feat: remove node from tree

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -37,4 +37,61 @@ class BinarySearchTree {
       return this;
     }
   }
+
+  remove(value) {
+    if (!this.root) return false;
+
+    let currentNode = this.root;
+    let parentNode = null;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        if (currentNode.right === null) {
+          if (parentNode === null) this.root = currentNode.left;
+          else {
+            if (currentNode.value < parentNode.value)
+              parentNode.left = currentNode.left;
+            else if (currentNode.value > parentNode.value)
+              parentNode.right = currentNode.right;
+          }
+        } else if (currentNode.right.left === null) {
+          currentNode.right.left = currentNode.left;
+          if (parentNode === null) this.root = currentNode.right;
+          else {
+            if (currentNode.value < parentNode.value)
+              parentNode.left = currentNode.right;
+            else if (currentNode.value > parentNode.value)
+              parentNode.right = currentNode.right;
+          }
+        } else {
+          let leftMost = currentNode.right.left;
+          let leftMostParent = currentNode.right;
+          while (leftMost.left !== null) {
+            leftMostParent = leftMost;
+            leftMost = leftMost.left;
+          }
+          leftMostParent.left = leftMost.right;
+          leftMost.left = currentNode.left;
+          leftMost.right = currentNode.right;
+
+          if (parentNode === null) this.root = leftMost;
+          else {
+            if (currentNode.value < parentNode.value)
+              parentNode.left = leftMost;
+            else if (currentNode.value > parentNode.value)
+              parentNode.right = leftMost;
+          }
+        }
+        return true;
+      }
+    }
+  }
 }
+const binaryTree = new BinarySearchTree();
+export default binaryTree;
